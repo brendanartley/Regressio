8fed61fb366cbea7aca0c6ac48330d15f0fe8cd4
Brendan <baa30@sfu.ca>
2022-07-21 11:25:48 -0700

    Added model.fit tests
    Added model.fit tests for the existing models.

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,13 +29,24 @@ class Test_linear_regression:
     def test_degree_less_than_10(self):
         with pytest.raises(ValueError):
             model = linear_regression(-1)
+    
+    def test_fit_model(self):
+        x, y = generate_random_walk(100)
+        model = linear_regression(degree=10)
+        model.fit(x, y)
             
 class Test_linear_interpolation: 
     def test_no_data_in_knot(self):
         with pytest.raises(ValueError):
             x, y = np.arange(20), np.cumsum(np.ones(20))
             model = linear_interpolation(21)
+            np.random.shuffle(x)
             model.fit(x, y)
+
+    def test_fit_model(self):
+        x, y = generate_random_walk(100)
+        model = linear_interpolation(knots=12)
+        model.fit(x, y)
 
 class Test_isotonic_regression: 
     def test_no_data_in_knot(self):
@@ -44,9 +55,19 @@ class Test_isotonic_regression:
             model = isotonic_regression(21)
             model.fit(x, y)
 
+    def test_fit_model(self):
+        x, y = generate_random_walk(100)
+        model = isotonic_regression(knots=12)
+        model.fit(x, y)
+
 class Test_bin_regression: 
     def test_no_data_in_knot(self):
         with pytest.raises(ValueError):
             x, y = np.arange(20), np.cumsum(np.ones(20))
             model = bin_regression(21)
             model.fit(x, y)
+
+    def test_fit_model(self):
+        x, y = generate_random_walk(100)
+        model = bin_regression(knots=8)
+        model.fit(x, y)
